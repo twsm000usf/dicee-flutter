eef818ccb5ca49ec89efdf872f02a12d1c6d067e
Thomas Weverton dos Santos Moura <thomas.moura@mail.usf.edu.br>
2019-12-31 21:56:48 -0300

    Add StatefulWidget, Buttons and functions

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
+
+var random = Random();
 
 void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.redAccent,
         appBar: AppBar(
           title: Text('Dicee'),
           backgroundColor: Colors.red,
@@ -15,9 +18,45 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    VoidCallback onPressEvent = () {
+      setState(() {});
+    };
+    return Center(
+      child: Row(
+        children: <Widget>[
+          getButtonExpanded(onPressEvent),
+          getButtonExpanded(onPressEvent),
+        ],
+      ),
+    );
   }
+}
+
+Widget getButtonExpanded(VoidCallback onPress) {
+  return Expanded(
+    flex: 1,
+    child: FlatButton(
+      onPressed: () {
+        onPress();
+      },
+      child: Image.asset(getDiceImageAsset()),
+    ),
+  );
+}
+
+int getRandomSixFaceDiceNumber() {
+  return random.nextInt(6) + 1;
+}
+
+String getDiceImageAsset() {
+  int number = getRandomSixFaceDiceNumber();
+  return 'images/dice$number.png';
 }
